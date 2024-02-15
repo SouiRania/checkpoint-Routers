8cde10208895cfcc84884e40cbdbe0268ae75acb
SouiRania <rania.souissi13@gmail.com>
2024-02-15 20:47:38 +0100

    i added routercheckpoint

--- src/App.js
+++ src/App.js
@@ -7,9 +7,9 @@ import { useState } from 'react';
 
 
 function App() {
-  const [list,setlist]=useState([{title:'FALLING IN LOVE',description:'When Gabriella shares a trip to a fair at the Citadine and acquires a rustic terrace in Nouvelle-Zélande, she sets it up with a kindly businessman, Jake Taylor, for repair and profit.',posterURL:'http://fr.web.img6.acsta.net/pictures/19/08/19/10/00/0449235.jpg',trailer:"https://www.youtube.com/embed/S-1QgOMQ-ls",rating:'8/10'},
-  {title:'SAW',description:'Saw is a 2004 American horror film directed by James Wan, in his feature directorial debut, and written by Leigh Whannell from a story by Wan and Whannell',posterURL:'https://www.themoviedb.org/t/p/original/mlllmFvodsry9QUIc3fT6Hf4pXd.jpg',trailer:'https://www.youtube.com/watch?v=S-1QgOMQ-ls',rating:'5/10'},
-  {title:'The Gold Rush',description:'In this classic silent comedy, the Little Tramp (Charles Chaplin) heads north to join in the Klondike gold rush.',posterURL:'https://image.tmdb.org/t/p/original/eQRFo1qwRREYwj47Yoe1PisgOle.jpg',trailer:'https://www.youtube.com/watch?v=kDlEvaKBkhU',rating:'6/10'}
+  const [list,setlist]=useState([{title:'FALLING IN LOVE',description:'When Gabriella shares a trip to a fair at the Citadine and acquires a rustic terrace in Nouvelle-Zélande, she sets it up with a kindly businessman, Jake Taylor, for repair and profit.',posterURL:'http://fr.web.img6.acsta.net/pictures/19/08/19/10/00/0449235.jpg',trailer:'https://www.youtube.com/embed/P9vXNloQfTM' ,type:'Romantic film',rating:'8/10'},
+  {title:'SAW',description:'Saw is a 2004 American horror film directed by James Wan, in his feature directorial debut, and written by Leigh Whannell from a story by Wan and Whannell',posterURL:'https://www.themoviedb.org/t/p/original/mlllmFvodsry9QUIc3fT6Hf4pXd.jpg',trailer:'https://www.youtube.com/embed/S-1QgOMQ-ls',type:'Horror film',rating:'5/10'},
+  {title:'The Gold Rush',description:'In this classic silent comedy, the Little Tramp (Charles Chaplin) heads north to join in the Klondike gold rush.',posterURL:'https://image.tmdb.org/t/p/original/eQRFo1qwRREYwj47Yoe1PisgOle.jpg',trailer:'https://www.youtube.com/embed/Srnlg2b7y7M',type:'Commedy film',rating:'6/10'}
   ])
 
 
--- src/Trailer.jsx
+++ src/Trailer.jsx
@@ -1,14 +1,20 @@
 import React from 'react'
-import { useParams } from 'react-router-dom'
+import { useParams, NavLink } from 'react-router-dom'
 
 function Trailer({list}) {
     
   const {id}=useParams()
   return (
-    <div>
+    <div class="p-3 mb-2  text-black">
+    
+    
+     
+    <h1>{list[id].type}</h1>
+    <iframe width="853" height="480" src={list[id].trailer} title="Saw (2004) Official Trailer #1 - James Wan Movie" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe><br/><br/>
+    <h6>{list[id].description}</h6> 
 
-    <iframe width="853" height="480" src={list[id].trailer} title="Saw (2004) Official Trailer #1 - James Wan Movie" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
-    <p>{list[id].description}</p>
+    <NavLink to={"/"} ><button type="button" class="btn-warning">Home</button>
+    </NavLink>
     </div>
   )
 }
